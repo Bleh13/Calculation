d2c09bb2c08d36b5f6bf7d2cc0c67e6889121436
Bleh13 <rselvamuthan@gmail.com>
2022-02-09 05:01:52 +0530

    filter

--- front_end/frontend/src/ViewInventory.jsx
+++ front_end/frontend/src/ViewInventory.jsx
@@ -3,6 +3,7 @@ import Accordion from '@mui/material/Accordion';
 import TextField from '@mui/material/TextField';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import MenuItem from '@mui/material/MenuItem'
+import { InputLabel } from '@mui/material';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import axios from 'axios';
 import { useState } from 'react'
@@ -18,9 +19,15 @@ import SearchIcon from '@mui/icons-material/Search';
 import { getTableSortLabelUtilityClass } from '@mui/material';
 
 export default function ViewInventory() {
-const Searchvalue=[{id:1,value:'Producttype'},{id:2,value:'ProductDescription'},{id:3,value:'Discount'},{id:4,value:'Price'}]
  const [data, setdata] = useState(true);
- const [selectedsearch,setSelectedsearch]=useState()
+ const [filtervalue,setfiltervalue] =useState({
+  ProductDescription:" " ,
+Producttype:"",
+quantity :"",
+Discount:0,
+Priceperunit:0
+ })
+ const Producttype= [{id:1,value:'vegie'},{id:2,value:'fruit'},{id:2,value:'addanother type'} ]
  const [x, setx] = useState([]);
  const { User } = useAuthState();
  var y=User.email
@@ -40,16 +47,64 @@ useEffect(() => {
   
   }
 , []);  
+function handleFilter(e){
+const{name,value}=e.target;
+setfiltervalue(previousstate=>({
+  ...previousstate,
+[name]:value
+}))
+}
+function HandleFilterSubmit(e){
+e.preventDefault();
+x.filter()
+}
 return(
   <div>
     {console.log(x)}
-    <TextField onChange={e=>{setSelectedsearch(e.target.value)}} select  label="Filter based on "  variant='filled' focused fullWidth multiline>
-{Searchvalue.map(option=>(
+    <Accordion square={false} >
+  <AccordionSummary   expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header">Filter</AccordionSummary>
+          <AccordionDetails sx={{display:'table'}}>
+          <InputLabel sx={{textAlign:'center' }}>Product Description</InputLabel> 
+     <Input name="ProductDescription" value={filtervalue.ProductDescription} type="string"
+      onChange={handleFilter()}
+      multiline  />    
+      <br></br>
+  <TextField 
+  // onChange={e=>{settype(e.target.value)}} 
+  select  label="type of product"    color='' variant='filled' focused fullWidth  multiline>
+{Producttype.map(option=>(
   <MenuItem key={option.id} value={option.value}>
     {option.value}
   </MenuItem>
 ))}
   </TextField>
+ <InputLabel >quantity</InputLabel>   
+ <Input type="number" name="ProductDescription" value={filtervalue.quantity} 
+      onChange={handleFilter()}  
+//  onChange={e=>setQuantity(e.target.value)}
+  />     
+ <br></br>
+ <InputLabel >Discount</InputLabel>   
+ <Input type="number"  name="ProductDescription" value={filtervalue.Discount} 
+      onChange={handleFilter()}
+//  onChange={e=>{
+//    setDiscount(e.target.value)}} 
+   />    
+    <br></br>
+ <InputLabel >Price per unit</InputLabel>   
+ <Input type="number" name="ProductDescription" value={filtervalue.Priceperunit} 
+      onChange={handleFilter()}
+  // onChange={calculate} 
+  />
+  <br></br>
+  <br></br>
+  <Button variant="contained" onSubmit={HandleFilterSubmit()}   >
+  Filter
+</Button>
+          </AccordionDetails >
+          </Accordion>
   <Input sx={{ ml:50 }} placeholder="search" type="string"  multiline >
    </Input>
    <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
